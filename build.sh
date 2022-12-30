a9a50bb5e2e2fe3a360dcb216c3babda8e84f2e3
Maik Marschner <an@maik.tk>
2022-12-30 20:32:36 +0100

    Fix loading versions, deploy to correct path.

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-rm -rf final-site
-mkdir final-site
+rm -rf docs
+mkdir docs
 cd ChunkyDocs
+
 CHUNKY_VERSION=20500 python -m mkdocs build
-cp -r site ../final-site/snapshot
+cp -r site ../docs/snapshot
+# get the versions from /docs/versions.json instead of the hard-coded ../versions.json
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's/..\/versions.json/\/docs\/versions.json/g' ../docs/snapshot/assets/javascripts/bundle*.min.js
+else
+    sed -i 's/..\/versions.json/\/docs\/versions.json/g' ../docs/snapshot/assets/javascripts/bundle*.min.js
+fi
+
 CHUNKY_VERSION=20404 python -m mkdocs build
-cp -r site/* ../final-site/
-echo '[{ "version": "..", "title": "2.4.4", "aliases": [] }, { "version": "snapshot", "title": "2.5.0 (snapshot)", "aliases": [] }]' > ../final-site/versions.json
+cp -r site/* ../docs/
+# get the versions from /docs/versions.json instead of the hard-coded ../versions.json
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's/..\/versions.json/\/docs\/versions.json/g' ../docs/assets/javascripts/bundle*.min.js
+else
+    sed -i 's/..\/versions.json/\/docs\/versions.json/g' ../docs/assets/javascripts/bundle*.min.js
+fi
+
+echo '[{ "version": "../docs", "title": "2.4.4", "aliases": ["stable"] }, { "version": "../docs/snapshot", "title": "2.5.0 (snapshot)", "aliases": ["snapshot"] }]' > ../docs/versions.json
